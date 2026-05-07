6f7a89c5ea0ec67d22d4e52365edb89f22e62181
Bilal Bahloul El Miri <bbahloul@a.iescanpeublanc.cat>
2026-05-07 10:26:56 +0200

    calculadora toni

---
+++ calculadora_project/calculadora/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+def calculadora_view(request):
+    resultado = None
+
+    if request.method == "POST":
+        num1 = float(request.POST.get("num1"))
+        num2 = float(request.POST.get("num2"))
+        operacion = request.POST.get("operacion")
+
+        if operacion == "suma":
+            resultado = num1 + num2
+        elif operacion == "resta":
+            resultado = num1 - num2
+        elif operacion == "multiplicacion":
+            resultado = num1 * num2
+        elif operacion == "division":
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                resultado = "Error: división por 0"
+
+    return render(request, "calculadora/calculadora.html", {"resultado": resultado})
